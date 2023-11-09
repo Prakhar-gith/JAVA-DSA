80c54be1377c22c74d044fe15c8d7b27940ccb03
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2023-11-09 20:12:17 +0530

    Day 42 - Search Peak element

---
+++ #100DaysOfCode/42SearchPeakElement.java
@@ -0,0 +1,68 @@
+
+/*
+ * Problem Link: https://leetcode.com/problems/find-peak-element/
+ * Problem:
+A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+nums[i] != nums[i + 1] for all valid i.
+Seen this question in a real interview before?
+1/4
+ */
+
+// Solution :-
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+
+        if (nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
+        }
+
+        int left = 1;
+        int right = nums.length - 2;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+
+            if (nums[mid] < nums[mid - 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
