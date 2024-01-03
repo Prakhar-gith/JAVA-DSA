7fa71499e5b8efd20e2fbae7255f8e94fe6444c8
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2024-01-03 23:16:01 +0530

    Problem - Sort People

---
+++ #100DaysOfCode/93SortPeople.java
@@ -0,0 +1,53 @@
+
+/*
+ * Problem Link: https://leetcode.com/problems/sort-the-people/
+ * Problem:
+ * You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+
+ 
+
+Example 1:
+
+Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+Output: ["Mary","Emma","John"]
+Explanation: Mary is the tallest, followed by Emma and John.
+Example 2:
+
+Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+Output: ["Bob","Alice","Bob"]
+Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+ 
+
+Constraints:
+
+n == names.length == heights.length
+1 <= n <= 103
+1 <= names[i].length <= 20
+1 <= heights[i] <= 105
+names[i] consists of lower and upper case English letters.
+All the values of heights are distinct.
+ 
+ */
+
+// Solution :-
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+        Arrays.sort(heights);
+        String[] sortedNames = new String[names.length];
+        int index = 0;
+        for (int i = heights.length - 1; i >= 0; i--) {
+            sortedNames[index] = map.get(heights[i]);
+            index++;
+        }
+        return sortedNames;
+    }
+}
