4fc7f2669a20bc67460a1b4db36d44693a482e9b
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2024-01-12 23:48:41 +0530

    Day - 95 Maximum Average Subarray I

--- #100DaysOfCode/95MaximumAverageSubarrayI.java
+++ #100DaysOfCode/95MaximumAverageSubarrayI.java
@@ -6,7 +6,6 @@
 
 Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
 
-
 Example 1:
 
 Input: nums = [1,12,-5,-6,50,3], k = 4
@@ -16,8 +15,6 @@ Example 2:
 
 Input: nums = [5], k = 1
 Output: 5.00000
- 
-
 Constraints:
 
 n == nums.length
